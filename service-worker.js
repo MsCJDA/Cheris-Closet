51e98af363634169c7ba0f9ac6d2ad067512611a
MsCJDA <Cheri.dunston@gmail.com>
2025-08-08 21:12:09 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='cheris-closet-static-v1';
+const urlsToCache=['/','/index.html','/manifest.json','/service-worker.js','/icons/icon-192.png','/icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache)))});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k)))))})
